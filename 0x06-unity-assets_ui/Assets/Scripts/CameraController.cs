d0b2f6006e7818c0668598f49d03ee6a29e8e83d
Brett Davis <brett.p.davis101@gmail.com>
2021-02-16 16:34:02 -0600

    Adds 0x06

---
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Player gameobject
+    public GameObject player;
+    // Camera offset from player
+    public Vector3 offset = new Vector3(0, 2.5f, -6.25f);
+    // Camera rotation spd
+    public float rotationSpd = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotationSpd;
+        // transform.position = player.transform.position + offset;
+        player.transform.Rotate(0, horizontal, 0);
+
+        float newAngle = player.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, newAngle, 0);
+        transform.position = player.transform.position + (rotation * offset);
+
+        transform.LookAt(player.transform);
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    // Timer text
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Timer script = other.gameObject.GetComponent<Timer>();
+        script.enabled = false;
+        winText.color = Color.green;
+        winText.fontSize = 70;
+    }
+}
